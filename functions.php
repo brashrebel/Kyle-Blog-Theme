1ad19c1eaa3294edff078065021ef10add91903b
brashrebel <kyle@realbigmarketing.com>
2014-07-26 10:14:16 -0400

    Making the SASS my own way
    Start with foundation css, add where I want it and build with Compass.

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function kyle_setup() {
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-	
+
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
@@ -104,6 +104,9 @@ function kyle_scripts() {
 	wp_enqueue_script( 'kyle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'kyle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	/* Add Foundation JS */
+	wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/js/foundation.min.js', array( 'jquery' ), '1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
