0e0aafa9997cda39fd7a02c4a7e2d9ee2ec7575b
Kyle Maurer <kyle@realbigmarketing.com>
2014-09-23 17:37:16 -0400

    Added a bunch of sidebar buttons. Also added dashicons to the front end.

--- functions.php
+++ functions.php
@@ -113,6 +113,7 @@ add_action( 'widgets_init', 'kyle_widgets_init' );
 function kyle_scripts() {
 	wp_enqueue_style( 'kyle-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'kfonts' );
+	wp_enqueue_style( 'dashicons' );
 
 	wp_enqueue_script( 'kyle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
--- sidebar.php
+++ sidebar.php
@@ -13,19 +13,77 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <div id="secondary" class="widget-area" role="complementary">
 	<h2>Hello, my name is Kyle and I like to...</h2>
 	<ul class="stack button-group">
-		<li><a href="#" class="button large">Button 1</a></li>
-		<li><a href="#" class="button large">Button 2</a></li>
 		<?php // get the categories
 		$categories = get_categories();
 		if ( $categories ) {
 			?>
-			<li><a href="#" class="button split expand">Blog about these topics <span data-dropdown="drop"></span></a></li>
+			<li><a href="#" class="button split expand"><div class="dashicons dashicons-rss"></div>Blog about these topics <span data-dropdown="drop"></span></a></li>
 			<ul id="drop" class="f-dropdown" data-dropdown-content>
 				<?php foreach ( $categories as $category ) {
 					echo '<li><a href="'. $category->slug .'">'. $category->name .'</a></li>';
 				} ?>
 			</ul>
 		<?php } ?>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-wordpress-alt"></div> Make WordPress plugins
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-lightbulb"></div> Run my business
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-twitter"></div> Engage others on the Twitters
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-businessman"></div> Make connections on LinkedIn
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-googleplus"></div> Waste time on Google+
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-media-code"></div> Share code on Github
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-nametag"></div> Attend local meetups
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-facebook"></div> Occasionally login to Facebook
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-format-audio"></div> Make Grooveshark playlists
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-megaphone"></div> Speak at WordPress events
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-microphone"></div> Podcast weekly
+			</a>
+		</li>
+		<li>
+			<a href="#" class="button large">
+				<div class="dashicons dashicons-welcome-learn-more"></div> Show off my knowledge
+			</a>
+		</li>
 	</ul>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
