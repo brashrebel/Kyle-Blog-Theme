d15aa803882afec2bdc28c62107f67169446136b
Kyle Maurer <kyle@realbigmarketing.com>
2017-12-08 13:18:27 -0500

    Fixed some image and body styling. Also removed a footer link and bumped version.

--- footer.php
+++ footer.php
@@ -14,8 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'kyle' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'kyle' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s', 'kyle' ), 'Designed', '<a href="http://realbigmarketing.com/staff/kyle" rel="designer">me</a>' ); ?>
-			 using <a href="http://foundation.zurb.com">Foundation</a>
+			Designed by me using <a href="http://foundation.zurb.com">Foundation</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
